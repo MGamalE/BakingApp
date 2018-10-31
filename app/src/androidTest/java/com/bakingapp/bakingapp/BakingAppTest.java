0a7944e8d7b9a04e67df3640e33e7639babda45b
mohamedelsharkawy <Mohamedgamal@fci.helwan.edu.eg>
2018-10-31 18:20:19 +0200

    update gradle

--- app/src/androidTest/java/com/bakingapp/bakingapp/BakingAppTest.java
+++ app/src/androidTest/java/com/bakingapp/bakingapp/BakingAppTest.java
@@ -1,11 +1,13 @@
 package com.bakingapp.bakingapp;
 
 import android.support.test.espresso.Espresso;
-import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.espresso.idling.CountingIdlingResource;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+
+
+import com.bakingapp.bakingapp.activity.MainActivity;
 
 import org.junit.After;
 import org.junit.Before;
@@ -14,8 +16,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 
+
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -25,7 +30,7 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 public class BakingAppTest {
 
     @Rule
-    public ActivityTestRule<com.bakingapp.bakingapp.activity.MainActivity> mActivityRule =
+    public ActivityTestRule<MainActivity> mActivityRule =
             new ActivityTestRule(com.bakingapp.bakingapp.activity.MainActivity.class);
 
 
